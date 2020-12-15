98a2817198e16051b8898b661490b24e4dbd84b0
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-15 18:18:02 +0900

    0

---
+++ src/com/javaex/ex01_2/PhoneApp.java
@@ -0,0 +1,14 @@
+package com.javaex.ex01_2;
+
+import java.io.IOException;
+
+public class PhoneApp {
+
+	public static void main(String[] args) throws IOException {
+		// mini project 전화번호 관리 프로그램 2
+		// 화면 표시 클래스, 데이터 클래스, 데이터관리 클래스, 메인으로 만들기
+
+		
+	}
+
+}
